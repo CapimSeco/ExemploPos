d227e3ebfecb987323314adeb0a2632bc884ae82
José Gustavo <doido3107@gmail.com>
2024-07-05 13:53:36 -0300

    comit inicial

--- exemplo.py
+++ exemplo.py
@@ -1,5 +1,4 @@
 print("Helow Word!")
 x = input("Digite um numero para somar: ")
 x1 = input("Digite o outro numero da soma: ")
-xr = "Sua resposta é = {x}{x1}"
-print({xr})
+print(f"Sua resposta é = {x}{x1}")
